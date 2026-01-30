90f8e5589dd2ffd3ae31b57e3a5a2834e091d494
przemekhys <48139301+przemekhys@users.noreply.github.com>
2026-01-30 23:52:47 +0100

    test(automation): add comprehensive unit tests for HomeAssistantAutomation controller

--- internal/controller/suite_test.go
+++ internal/controller/suite_test.go
@@ -31,6 +31,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	hav1alpha1 "github.com/przemekhys/homeassistant-operator/api/v1alpha1"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -57,6 +59,8 @@ var _ = BeforeSuite(func() {
 	ctx, cancel = context.WithCancel(context.TODO())
 
 	var err error
+	err = hav1alpha1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
 	// +kubebuilder:scaffold:scheme
 
 	By("bootstrapping test environment")
